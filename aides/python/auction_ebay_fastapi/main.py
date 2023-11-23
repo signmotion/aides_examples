5538ffb483d5d25057f686d07913c138a4b4e7f8
Andrii Syrokomskyi <signmotion@gmail.com>
2023-11-23 19:00:51 +0100

    [ABP-27] Removed a prefix `get` and unused imports. Added conventions.

--- aides/python/auction_ebay_fastapi/main.py
+++ aides/python/auction_ebay_fastapi/main.py
@@ -1,43 +1,56 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
+# * Conventions
+# ---------------
+#   * Always skip the verb for get-functions.
+#   * Order for declared endpoins:
+#     * root:
+#       * /about
+#       * /tags
+#       * /
+#     * endpoints:
+#       * /end/point/about
+#       * /end/point/tags
+#       * /end/point
+#       * /end/point/configure
 
-
-@app.get("/")
-def get():
-    return get_about()
+# For complex aide the best practic it's best to use recommendations
+# by link https://fastapi.tiangolo.com/tutorial/bigger-applications
+app = FastAPI(title="Auction eBay Aide")
 
 
 @app.get("/about")
-def get_about():
+def root_about():
     return "The aide for work with the eBay's auction."
 
 
 @app.get("/tags")
 # Tags for detect this aide.
 # There is no need to repeat the tags below for your own functions.
-def get_tags():
+def tags():
     return ["auction", "eBay"]
+
+
+@app.get("/")
+def root():
+    return root_about()
 
 
 @app.get("/products/today/about")
 # Description of this function.
-def get_products_today_about():
+def products_today_about():
     return "Returns a list of auction products no later than the last 24 hours."
 
 
 @app.get("/products/today/tags")
 # Tags for fast detect a needed function.
-def get_products_today_tags():
+def products_today_tags():
     return ["hours", "items", "now", "products", "today"]
 
 
 @app.get("/products/today")
 # See [get_products_today_about], [get_products_today_tags].
-def get_products_today():
+def products_today():
     # TODO
     return {
         "result": [
@@ -54,7 +67,7 @@ def get_products_today():
 @app.get("/products/today/configure")
 # The parameters for this function.
 # See examples on https://openai.com/blog/function-calling-and-other-api-updates
-def get_products_today_configure():
+def products_today_configure():
     return {
         "type": "object",
         "properties": {
@@ -65,7 +78,7 @@ def get_products_today_configure():
             },
             "location": {
                 "type": "string",
-                "about": "The country or city and state, e.g. Canand or San Francisco, CA."
+                "about": "The country or city and state, e.g. Canada or San Francisco, CA."
             },
         },
         "required": ["location"]
