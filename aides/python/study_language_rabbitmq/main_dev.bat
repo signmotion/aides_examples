722c2f448791e2589cb814d83278acf8d0c39d0b
Andrii Syrokomskyi <signmotion@gmail.com>
2023-12-19 12:42:54 +0100

    [ABP-12] Added starting a docker into main_dev.bat.

--- aides/python/study_language_rabbitmq/main_dev.bat
+++ aides/python/study_language_rabbitmq/main_dev.bat
@@ -1,11 +1,15 @@
 @echo !) Run the commands below after run `.\venv\Scripts\activate`
 
+@echo Starting the Docker...
+start "" /B "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+timeout /t 6 /nobreak
+
 @echo Stopping the Savant...
 docker stop rabbitmq
 
 @echo Starting the Savant...
 start "Savant"     docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.12-management
-timeout /t 12
+timeout /t 12 /nobreak
 
 @echo Starting the side-servers of aide...
 start "Appearance" uvicorn kins.main:appearance --factory --reload --port 12001
