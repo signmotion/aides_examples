9f7d0d02cf44a6e8be51669f33463ea84d5ef426
Andrii Syrokomskyi <signmotion@gmail.com>
2023-12-20 12:46:57 +0100

    [ABP-31] Refactoring.

--- aides/python/study_language_rabbitmq/kins/share/routes/phrasal_verbs.py
+++ aides/python/study_language_rabbitmq/kins/share/routes/phrasal_verbs.py
@@ -1,9 +1,10 @@
 import openai
 import re
 import traceback
-from typing import Any, Callable, Dict
+from typing import Any, Callable, Dict, Optional
 
 from ..config import *
+from ..context import Context
 from ..packages.aide_server.src.aide_server.log import logger
 from ..packages.aide_server.src.aide_server.task import Result, Task
 
@@ -29,23 +30,22 @@ async def phrasal_verbs(
         # "Chatgpt4Online",
     ]
 
-    responseResult = None
-    improvedResult = None
-    mappedResult = None
-    error = None
+    mapped_result: Optional[Dict[str, Any]] = None
+    response_result: Optional[Dict[str, Any]] = None
+    improved_result: Optional[Dict[str, Any]] = None
+    error: Optional[Exception] = None
     while True:
         try:
-            responseResult = _queryChatGpt(task.context)
+            response_result = _queryChatGpt(Context.model_validate(task.context))
 
             if improve_answer:
-                improvedResult = _improve(responseResult)  # type: ignore[override]
+                improved_result = _improve(response_result)  # type: ignore[override]
 
             if map_answer:
-                mappedResult = _map(
-                    improvedResult if improvedResult else _improve(responseResult)  # type: ignore[override]
+                mapped_result = _map(
+                    improved_result if improved_result else _improve(response_result)  # type: ignore[override]
                 )
 
-            error = None
             break
 
         except Exception as ex:
@@ -63,11 +63,11 @@ async def phrasal_verbs(
                 break
 
     value = _construct_answer(
-        mappedResult=mappedResult,  # type: ignore[override]
-        improvedResult=improvedResult,  # type: ignore[override]
-        rawResult=responseResult,  # type: ignore[override]
-        context=task.context,
-        error=error,  # type: ignore[override]
+        mapped_result=mapped_result,
+        improved_result=improved_result,
+        raw_result=response_result if include_raw_response_in_answer else None,
+        context=task.context if include_context_in_answer else None,
+        error=error,
     )
 
     await publish_progress(task=task, progress=100)
@@ -78,14 +78,14 @@ async def phrasal_verbs(
     )
 
 
-def _queryChatGpt(context: Dict[str, Any]):
+def _queryChatGpt(context: Context) -> Dict[str, Any]:
     if fake_response:
-        return _phrasal_verbs_demo_text(context)["result"]
+        return _phrasal_verbs_demo_text(Context.model_validate(context))["result"]
 
     openai.api_key = open_api_key
     response = openai.Completion.create(  # type: ignore
         engine="text-davinci-003",
-        prompt=_prompt(context["text"]),
+        prompt=_prompt(context.text),
         temperature=0.0,
         max_tokens=300,
     )
@@ -126,7 +126,7 @@ def _get_provider_from_error(ex: Exception):
 # ...
 # 37. think of (translation: думати про)\n
 # ...
-def _improve(text: str):
+def _improve(text: str) -> str:
     r = []
     lines = text.split("\n")
     for line in lines:
@@ -186,7 +186,7 @@ def _improve_line(line: str):
     return line.lower() if line else line
 
 
-def _map(improvedText: str):
+def _map(improvedText: str) -> Dict[str, Any]:
     r = {}
 
     lines = improvedText.split("\n")
@@ -198,22 +198,25 @@ def _map(improvedText: str):
 
 
 def _construct_answer(
-    mappedResult: dict,
-    improvedResult: dict,
-    rawResult: dict,
-    context: Dict[str, Any],
-    error: Exception,
-):
+    mapped_result: Optional[Dict[str, Any]] = None,
+    improved_result: Optional[Dict[str, Any]] = None,
+    raw_result: Optional[Dict[str, Any]] = None,
+    context: Optional[Dict[str, Any]] = None,
+    error: Optional[Exception] = None,
+) -> Dict[str, Any]:
     o = {}
 
-    if mappedResult:
-        o["mapped_result"] = mappedResult
+    if bool(mapped_result):
+        o["mapped_result"] = mapped_result
 
-    if improvedResult:
-        o["improved_result"] = improvedResult
+    if bool(improved_result):
+        o["improved_result"] = improved_result
 
-    if include_raw_response_in_answer or (not mappedResult and not improvedResult):
-        o["raw_result"] = rawResult
+    if bool(raw_result) or (not bool(mapped_result) and not bool(improved_result)):
+        o["raw_result"] = raw_result
+
+    if bool(context):
+        o["context"] = context
 
     if error:
         logger.error(error)
@@ -222,27 +225,24 @@ def _construct_answer(
             "traceback": f"{traceback.format_exc()}",
         }
 
-    if include_context_in_answer:
-        o["context"] = context
-
     return o
 
 
-def _phrasal_verbs_demo_text(context: Dict[str, Any]):
+def _phrasal_verbs_demo_text(context: Context) -> Dict[str, Any]:
     return {
         "result":
         # "1. make out (translation: зрозуміти)\n2. read out (translation: прочитати)\n3. come out (translation: вийти)\n4. use up (translation: використовувати)\n5. work out (translation: розібратися)\n6. figure out (translation: з'ясувати)\n7. go into (translation: увійти)\n8. read up on (translation: почитати про)\n9. set in (translation: встановлювати)\n10. make predictions (translation: робити передбачення)\n11. be good (translation: бути хорошим)\n12. be bad (translation: бути поганим)\n13. work well together (translation: добре співпрацювати разом)\n14. spend hours (translation: проводити години)\n15. love (translation: любити)\n16. hate (translation: ненавидіти)\n17. cause to pause (translation: заставляти задуматися)\n18. be useful (translation: бути корисним)\n19. be better than (translation: бути кращим, ніж)\n20. be worth it (translation: бути вартим того)\n21. figure things out (translation: розібратися в чомусь)\n22. have a disadvantage (translation: мати недолік)\n23. see something out (translation: побачити щось до кінця)\n24. know what something should do (translation: знати, що має робити щось)\n25. have a leg up on (translation: мати перевагу над)\n26. make accurate predictions (translation: робити точні передбачення)\n27. hinder from (translation: заважати чому-небудь)\n28. gloss over (translation: пропустити, замовчати)\n29. have a distinct advantage (translation: мати виразну перевагу)\n30. play a lot of (translation: грати багато в)\n31. help (translation: допомагати)\n32. have some familiarity with (translation: мати певну знайомість з)\n33. matter (translation: мати значення)\n34. provide meaningful value (translation: надавати практичну цінність)\n35. bring in (translation: привертати)\n36. get excited about (translation: захоплюватися)\n37. think of (translation: думати про)\n38. demand (translation: вимагати)\n39. learn (translation: вчитися)\n40. accomplish (translation: досягати)\n41. pull away from (translation: відводити увагу від)\n42. ask for (translation: просити)\n43. come up with (translation: придумувати)\n44. work on (translation: працювати над)\n45. create (translation: створювати)\n46. plot out (translation: розробляти схему)\n47. doubt (translation: сумніватися)\n48. happen next (translation: трапитися далі)\n49. find (translation: знаходити)\n50. struggle with (translation: боротися з)\n51. make decisions (translation: приймати рішення)\n52. understand (translation: розуміти)"
         "1. Take into account - Враховувати\n2. Write down - Записати\n3. Make out - Розібратися\n4. Read out - Вголос прочитати\n5. Work out - Розібратися (про щось)\n6. Come out - Вийти (про новий набір карт)\n7. Figure out - Розібратися\n8. Read up - Почитати про щось\n9. Love these things - Обожнювати це\n10. Hate them - Ненавидіти їх\n11. Paused - Зупинитись на мить\n12. Be worth it - Бути вартим\n13. Hinder from - Заважати\n14. Gloss over - Пропустити, не зупинятись\n15. Have a leg up on - Мати перевагу над\n16. Seem good - Здаватися хорошим\n17. Work out well - Вдалий результат\n18. Over-perform expectations - Перевиконувати очікування\n19. Get in the way - Заважати, стояти на шляху\n20. Look like - Виглядати як\n21. Provide meaningful value - Надавати суттєву користь\n22. Bring in - Привертати\n23. Pull away from - Відводити увагу від\n24. Come up with - Придумати\n25. Work on - Працювати над\n26. Plot out - Складати план\n27. Doubt it - Сумніватись в цьому\n28. Make good decisions - Приймати правильні рішення\n29. Studying in the moment - Вчитися в даний момент\n30. Understand right now - Розуміти зараз",
-        "context": context,
+        "context": context.model_dump_json(),
     }
 
 
-def _phrasal_verbs_demo_json(context: Dict[str, Any]):
+def _phrasal_verbs_demo_json(context: Context) -> Dict[str, Any]:
     return {
         "result": {
             "draft (cards)": "вибирати (карти)",
             "come up with": "придумати",
             "pull away": "відволікати",
         },
-        "context": context,
+        "context": context.model_dump_json(),
     }
