78a1b498006e464b6f700dda54cc14b5b539905e
Andrii Syrokomskyi <signmotion@gmail.com>
2023-12-22 16:09:38 +0100

    [AU-4] Fixed short_json.

--- aides/python/buy_on_ebay_rabbitmq/kins/share/packages/short_json/src/short_json/short_json.py
+++ aides/python/buy_on_ebay_rabbitmq/kins/share/packages/short_json/src/short_json/short_json.py
@@ -1,18 +1,23 @@
 import json
+import logging
 from pydantic import BaseModel
-import textwrap
-from typing import Any, Dict, Set, Union
+from typing import Any, Dict, List, Set, Union
+
+
+logger = logging.getLogger("uvicorn.error") or logging.root
 
 
 def short_json(
-    o: Union[BaseModel, Dict[str, Any], str],
-    truncate_string_to_length: int = 120,
+    o: Union[BaseModel, Dict[str, Any], List[Any], Set[Any], str, int, float, None],
+    slice_dict_to_length: int = 12,
+    slice_list_to_length: int = 4,
+    slice_str_to_length: int = 120,
     include: Union[Set[int], Set[str], Dict[int, Any], Dict[str, Any], None] = None,
     exclude: Union[Set[int], Set[str], Dict[int, Any], Dict[str, Any], None] = None,
     exclude_unset: bool = True,
     exclude_defaults: bool = False,
     exclude_none: bool = True,
-) -> Dict[str, Any]:
+) -> Union[Dict[str, Any], List[Any], Set[Any], str, int, float, None]:
     """
     Truncate the values into JSON, exclude unused, empty or default values.
 
@@ -27,7 +32,8 @@ def short_json(
     Returns:
         A short JSON string representation.
     """
-    d = o
+
+    d: Union[Dict[str, Any], List[Any], Set[Any], str, int, float, None]
     if isinstance(o, BaseModel):
         d = o.model_dump(
             include=include,
@@ -37,18 +43,40 @@ def short_json(
             exclude_none=exclude_none,
         )
     elif isinstance(o, str):
-        d = json.loads(o)
+        try:
+            d = json.loads(o)
+        except Exception as _:
+            d = o
+    else:
+        d = o
 
-    # work with `dict`
-    r: Dict[str, Any] = {}
-    for k, v in d.items():  # type: ignore[override]
-        if isinstance(v, dict):
-            r[k] = short_json(v)
-        elif isinstance(v, str):
-            r[k] = textwrap.shorten(
-                v,
-                width=truncate_string_to_length,
-                placeholder="...",
-            )
+    if isinstance(d, str):
+        if slice_str_to_length > 0 and len(d) > slice_str_to_length:
+            d = f"{d[:slice_str_to_length]}..."
+        return d
+
+    if isinstance(d, dict):
+        d = list(d.items())
+        if slice_dict_to_length > 0 and len(d) > slice_dict_to_length:
+            d = d[slice(slice_dict_to_length)]
+            d.append(("...", "..."))
+        return dict((k, short_json(v)) for k, v in d)
+
+    if isinstance(d, list):
+        d = _sliced(d, slice_list_to_length)
+        return [short_json(v) for v in d]
+
+    if isinstance(d, set):
+        d = _sliced(d, slice_list_to_length)
+        return set([short_json(v) for v in d])
+
+    return d
+
+
+def _sliced(v: Union[List[Any], Set[Any]], limit: int) -> List[Any]:
+    r: List[Any] = v if isinstance(v, list) else list(v)
+    if limit > 0 and len(v) > limit:
+        r = r[slice(limit)]
+        r.append("...")
 
     return r
