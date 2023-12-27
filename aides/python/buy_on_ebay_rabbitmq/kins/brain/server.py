433aeffa2ce52fdbeff94e463a4ea5344fe7e6c3
Andrii Syrokomskyi <signmotion@gmail.com>
2023-12-27 12:11:17 +0100

    [AU-4] Renamed folder `routers` to `acts`.

--- aides/python/buy_on_ebay_rabbitmq/kins/brain/server.py
+++ aides/python/buy_on_ebay_rabbitmq/kins/brain/server.py
@@ -1,6 +1,6 @@
 from ..share.config import *
 from ..share.packages.aide_server.src.aide_server.server import AideServer
-from ..share.routers.products_today import products_today
+from ..share.acts.products_today import products_today
 
 
 class Brain(AideServer):
